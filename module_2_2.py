696df9555b3ca7b7de9f39a70facafb3963be087
TheBrain256 <Shimoro2576@gmail.com>
2024-10-19 15:49:34 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,29 @@
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second and third == first:
+    print(3)
+elif first == second or third == first:
+    print(2)
+else:
+    print(0)
+
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second and third == first:
+    print(3)
+elif first == second or third == first:
+    print(2)
+else:
+    print(0)
+
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second and third == first:
+    print(3)
+elif first == second or third == first:
+    print(2)
+else:
+    print(0)
